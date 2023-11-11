c24a5b7cc1405d2273f0909bf4152c7b5a32b103
Danilo <123840656+DaniloIvk@users.noreply.github.com>
2023-11-11 01:44:43 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", checkThemeChange);
 
+function checkThemeChange() {
+    let colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    let colorSchemeChangeHandler = event => {
+        if (event.matches) {
+            document.documentElement.className = 'dark';
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.className = 'light';
+            localStorage.setItem('theme', 'light');
+        }
+    }
+    colorSchemeQueryList.addEventListener('change', colorSchemeChangeHandler);
+    let storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        document.documentElement.className = storedTheme;
+    } else if (colorSchemeQueryList.matches) {
+        document.documentElement.className = 'dark';
+    } else {
+        document.documentElement.className = 'light';
+    }
+}
+
+document.getElementById('themeButton').addEventListener('click', function() {
+    if (document.documentElement.className === 'dark') {
+        document.documentElement.className = 'light';
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.className = 'dark';
+        localStorage.setItem('theme', 'dark');
+    }
+});
